8cad7e09e6f3064aa1eb14f613d38dcd8c07d328
Corax <corax@inbox.ru>
2025-01-10 19:37:35 +0400

    Initial Commit

---
+++ Kumihimo/KumihimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KumihimoApp.swift
+//  Kumihimo
+//
+//  Created by Дмитрий on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct KumihimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
