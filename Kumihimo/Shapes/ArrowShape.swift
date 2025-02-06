b6ee4418f01cf79bb83e868fd069427f687d7c2f
Corax <corax@inbox.ru>
2025-02-06 19:51:49 +0400

    Add arrowShape for inner MoveType, add 8k scheme

--- Kumihimo/Shapes/ArrowShape.swift
+++ Kumihimo/Shapes/ArrowShape.swift
@@ -19,16 +19,31 @@ struct ArrowShape: Shape {
 		
 		var path = Path()
 		path.move(to: from)
-		path.addLine(to: to)
-		let angle = atan2(to.y - from.y, to.x - from.x)
-		let arrowLength: CGFloat = 5
-		let arrowPoint1 = CGPoint(x: to.x - arrowLength * cos(angle - .pi / 6), y: to.y - arrowLength * sin(angle - .pi / 6))
-		let arrowPoint2 = CGPoint(x: to.x - arrowLength * cos(angle + .pi / 6), y: to.y - arrowLength * sin(angle + .pi / 6))
 		
-		path.move(to: arrowPoint1)
-		path.addLine(to: to)
-		path.addLine(to: arrowPoint2)
-		
+		switch move.type {
+		case .line:
+			path.addLine(to: to)
+			let angle = atan2(to.y - from.y, to.x - from.x)
+			let arrowLength: CGFloat = 5
+			let arrowPoint1 = CGPoint(x: to.x - arrowLength * cos(angle - .pi / 6), y: to.y - arrowLength * sin(angle - .pi / 6))
+			let arrowPoint2 = CGPoint(x: to.x - arrowLength * cos(angle + .pi / 6), y: to.y - arrowLength * sin(angle + .pi / 6))
+			
+			path.move(to: arrowPoint1)
+			path.addLine(to: to)
+			path.addLine(to: arrowPoint2)
+		case .inner:
+			let control1: CGPoint = CGPoint(x: from.x / 3, y: from.y / 3)
+			let control2: CGPoint = CGPoint(x: to.x / 3, y: to.y / 3)
+			path.addCurve(to: to, control1: control1, control2: control2)
+			let angle = atan2(to.y - control2.y, to.x - control2.x)
+			let arrowLength: CGFloat = 5
+			let arrowPoint1 = CGPoint(x: to.x - arrowLength * cos(angle - .pi / 6), y: to.y - arrowLength * sin(angle - .pi / 6))
+			let arrowPoint2 = CGPoint(x: to.x - arrowLength * cos(angle + .pi / 6), y: to.y - arrowLength * sin(angle + .pi / 6))
+			
+			path.move(to: arrowPoint1)
+			path.addLine(to: to)
+			path.addLine(to: arrowPoint2)
+		}
 		return path
 	}
 }
